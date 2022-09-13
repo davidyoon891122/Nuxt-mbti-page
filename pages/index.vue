6003f1b3446f54aa17f6a68d0ebe75f89433a773
davidyoon <qqpo12@naver.com>
2022-09-13 21:25:40 +0900

    Add components for mbti page
    * Added Button, Progress, Quiz components
    * Removed useless code Tutorial, NuxtLogo
    * Added vue skeleton of vue on _mbti.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,34 @@
 <template>
-  <Tutorial/>
+  <!-- 인트로 페이지 -->
+  <main v-if="page === 0"></main>
+  <!-- 퀴즈 페이지 -->
+  <main v-else></main>
 </template>
 
 <script>
+/*
+* 메인 페이지 않에 들어갈 것
+* 1. 인트로 페이지
+* 2. 조건부 렌더링을 통해 퀴즈 컴포넌트
+* 3. 결과는 페이지 이동 /result/_mbti
+*/
 export default {
-  name: 'IndexPage'
+  computed: {
+    page() {
+      return 0
+    },
+    quiz() {
+      return []
+    }
+  }
 }
 </script>
+
+<style>
+  main {
+    padding: 30px 0;
+    width: 100%;
+    max-width: 360px;
+    margin: auto;
+  }
+</style>
